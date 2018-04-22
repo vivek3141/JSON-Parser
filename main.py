80babe8e2f9e8ccdb16c79a8d5cf4af5f648732e
NutriScan <scannutri@gmail.com>
2018-04-21 17:48:43 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,3 @@
+import sys
+import parse
+print(parse.parse(sys.argv[1]))
---
+++ parse.py
@@ -0,0 +1,57 @@
+#JSON Parser
+def parse(string):
+	string.replace("\n","")
+	#print(string)
+	#print(string[0])
+	#if not(string[0] == "{" and string[-1] == "}"):
+	#	return False
+	#string = string[1:-1]
+	ret = {}
+	temp = ""
+	key = ""
+	val = ""
+	k = 0
+	l = len(string)
+	i = ""
+	while(k<l):
+		#print(ret)
+		#print(i + " " + str(k) + " " + str(l))	
+		l = len(string)
+		if string == "{":
+			return ret
+		i = string[k]
+		if(i == ","):
+			val = temp[:]
+			ret[key]=val
+			temp = ""
+			key = ""
+			val = ""
+			k += 1
+			continue
+		if(i == ":"):
+			key = temp[:]
+			#print(key)
+			temp = ""
+			k += 1
+			continue	
+		if(i == "{"):
+			key = temp
+			#temp = ""
+			test = string[(k+1):]
+			#print(test)
+			if("{" in string.replace("{","") and "}" in string.replace("}","")):
+				ret[key] = parse(test)
+			else:
+				#print("aA")
+				ret = parse(test)
+			string = string.replace(test,"")
+			#print("String :" + string)
+			val = ""
+			key = ""
+		if(i == "}"):
+			ret[key] = temp
+			return ret
+		temp = temp + str(i)
+		k += 1
+	ret[key] = temp
+	return ret	
