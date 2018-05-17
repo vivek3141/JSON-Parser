40736094faf121f4a31fdfcddf3d04c0358bc858
Vivek Verma <vivnps.verma@gmail.com>
2018-05-17 12:32:47 -0700

    Trying recursion

--- parse.py
+++ parse.py
@@ -1,3 +1,5 @@
+import re
+
 #JSON Parser
 def parse(string):
 	string.replace("\n","")
@@ -14,9 +16,14 @@ def parse(string):
 		l = len(string)
 		if string == "{":
 			return ret
-		i = string[k]
+		#print(string)
+		try:
+			i = string[k]
+		except IndexError:
+			print(string)
+			return ret
 		if(i == ","):
-			val = temp[:]
+			val = temp
 			ret[key]=val
 			temp = ""
 			key = ""
@@ -24,21 +31,43 @@ def parse(string):
 			k += 1
 			continue
 		if(i == ":"):
-			key = temp[:]
-			#print(key)
+			key = temp
 			temp = ""
 			k += 1
+			if(string[k+1] == "{"):
+				test = string[(k+1):]
+				#print(test)
+				li = [m.start() for m in re.finditer('}', test)]
+				#print(li)
+				v = ""
+				li2 = [m.start() for m in re.finditer('{', test)]
+				#print(li2)
+				num = -1			
+				for i in test:
+					if(i == "{"):
+						num = num + 1
+					if(i == "}" and num == 0):
+						v = v + i
+						break
+					if(i == "}"):
+						num = num - 1
+					v = v + i
+				#print(v)
+				temp = parse(v)
+				print(v)
+				string = string.replace(v,"")
+			#print(key)
 			continue	
 		if(i == "{"):
 			key = temp
 			#temp = ""
 			test = string[(k+1):]
-			#print(test)
-			if("{" in string.replace("{","") and "}" in string.replace("}","")):
-				ret[key] = parse(test)
-			else:
-				#print("aA")
-				ret = parse(test)
+			#index = test.find(",")
+			#print(index)
+			#if("{" in string.replace("{","") and "}" in string.replace("}","")):
+			#	ret[key] = parse(test)
+			#else:
+			ret = parse(test)
 			string = string.replace(test,"")
 			#print("String :" + string)
 			val = ""
@@ -48,5 +77,6 @@ def parse(string):
 			return ret
 		temp = temp + str(i)
 		k += 1
+	#print(ret)
 	ret[key] = temp
 	return ret	
